6a85fb08204f29a55d5f733b923700ac6ad85668
Janardhanam <JANARDHANAM@housing.wisc.edu>
2019-07-05 14:54:48 -0500

    renamed variables to more accurately reflect purpose

--- renderer.js
+++ renderer.js
@@ -7,9 +7,9 @@ const {
 } = require('electron')
 
 let KBcount = -1;
-let DLcount = -1;
-let msg;
-let count;
+let downloaded = -1;
+let DLstatus;
+let DLcount;
 
 // --Load events below-- //
 function addLoadEvent(func) {
@@ -34,15 +34,15 @@ addLoadEvent(autofocusBox);
 
 // --Load events above-- //
 
-document.getElementById('submit').addEventListener('click', function () {
+document.getElementById('download').addEventListener('click', function () {
 
     let LIST = document.getElementById("box").value;
-    document.getElementById('submit').style.display = 'none';
-    msg = document.getElementById('msg');
-    count = document.getElementById('count');
+    document.getElementById('download').style.display = 'none';
+    DLstatus = document.getElementById('DLstatus');
+    DLcount = document.getElementById('DLcount');
 
-    msg.style.display = 'block';
-    count.style.display = 'block';
+    DLstatus.style.display = 'block';
+    DLcount.style.display = 'block';
 
     // send username to main.js 
     ipcRenderer.send('LIST', LIST);
@@ -51,42 +51,42 @@ document.getElementById('submit').addEventListener('click', function () {
 // Show user that KBs are being located and show progress
 ipcRenderer.on('locating', (event, args) => {
     if (KBcount < 0) {
-        msg.innerHTML = 'Locating...'
+        DLstatus.innerHTML = 'Locating...'
         KBcount = args;
-        count.innerHTML = `(0/${KBcount})`;
+        DLcount.innerHTML = `(0/${KBcount})`;
     } else {
-        count.innerHTML = `(${args}/${KBcount})`;
+        DLcount.innerHTML = `(${args}/${KBcount})`;
     }
 });
 
 // Show user that KBs are being verified (writing legend file) and show progress
 ipcRenderer.on('verifying', (event, args) => {
-    msg.innerHTML = 'Verifying...';
-    count.innerHTML = `(${args}/${KBcount})`;
+    DLstatus.innerHTML = 'Verifying...';
+    DLcount.innerHTML = `(${args}/${KBcount})`;
 });
 
 // Show user that KBs are being downloaded and show progress
 ipcRenderer.on('downloading', (event, args) => {
-    if (DLcount == -1) {
-        msg.innerHTML = 'Downloading...';
-        DLcount = args;
-        count.innerHTML = `(0/${DLcount})`;
+    if (downloaded == -1) {
+        DLstatus.innerHTML = 'Downloading...';
+        downloaded = args;
+        DLcount.innerHTML = `(0/${downloaded})`;
     } else {
-        count.innerHTML = `(${args}/${DLcount})`;
+        DLcount.innerHTML = `(${args}/${downloaded})`;
     }
 });
 
-// Display close button
+// Display names button and display done!
 ipcRenderer.on('DONE', (event, args) => {
-    msg.innerHTML = 'Done!';
+    DLstatus.innerHTML = 'Done!';
     if (args == 1337){
-        count.innerHTML = 'nothing to download';
+        DLcount.innerHTML = 'nothing to download';
     } else {
-        count.style.display = 'none';
+        DLcount.style.display = 'none';
     }
-    document.getElementById('close').style.display = 'block';
+    //TODO: download again button
 });
 
-document.getElementById('close').addEventListener('click', function () {
-    ipcRenderer.send('CLOSE',0);
+document.getElementById('names').addEventListener('click', function () {
+    ipcRenderer.send('names.txt',0);
 });
