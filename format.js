2262329adb081e67333e1194f14eda66fc3ed829
Janardhanam <JANARDHANAM@housing.wisc.edu>
2019-07-10 12:12:21 -0500

    renamed organize.html/js to format.html/js Added UI to format.html

---
+++ format.js
@@ -0,0 +1,18 @@
+const { ipcRenderer } = require('electron');
+const { shell } = require('electron').remote;
+
+const path = "\\\\fs-hsg-1\\IT Department\\KB PDF2HTML\\";
+
+
+document.getElementById('organize').addEventListener('click', () => {
+    document.getElementById('formatter').style.display='none';
+    ipcRenderer.send('organize');
+});
+
+ipcRenderer.on('organize-done', (event,args)=>{
+    ipcRenderer.send('beautipy');
+});
+
+ipcRenderer.on('beautipy-done', (events,args) => {
+    //Done!
+});
