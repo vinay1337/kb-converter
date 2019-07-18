34c327ddcf0a449dbac937fac4eaffd8a658a231
Janardhanam <JANARDHANAM@housing.wisc.edu>
2019-07-18 16:56:24 -0500

    Added buttons that will do things eventually

--- upload.js
+++ upload.js
@@ -6,3 +6,16 @@ const path = "\\\\fs-hsg-1\\IT Department\\KB PDF2HTML\\";
 document.getElementById('outputDir').addEventListener('click', function () {
     shell.openItem(path + 'Organized');
 });
+
+
+document.getElementById('adminView').addEventListener('click', () => {
+    let KBID = document.getElementById('KBIDtxt').value;
+    shell.openExternal('https://kb.wisc.edu/kbAdmin/document.php?id=' + KBID);
+});
+
+document.getElementById("KBIDtxt").addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("adminView").click();
+    }
+});
